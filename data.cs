0068dd57a793dcfe664146b3f0b4b19990aa71c4
gabrielm1709 <gabriel.morck@elev.ga.ntig.se>
2022-12-07 11:02:51 +0100

    initial commit

---
+++ data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace oop1
+{
+    class data
+    {
+        List<Tuple<string, string>> memberList = new List<Tuple<string, string>>();
+        List<Tuple<string, string>> librarianList = new List<Tuple<string, string>>();
+        List<Tuple<string, int>> bookList = new List<Tuple<string, int>>();
+    }
+}
---
+++ user.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop1
+{
+    class User
+    {
+        public string name;
+        public string password;
+
+        public void addUser() 
+        {
+            
+        }
+    }
+
+    class Member : User 
+    {
+        public Member(string memberName, string pw) 
+        {
+            this.name = memberName;
+            this.password = pw;
+
+            if()
+        }
+    }
+
+    class Librarian : User 
+    {
+        private string code = "5678";
+        private bool success;
+        public Librarian(string librarianName, string pw, string access) 
+        {
+            if (access == code)
+            {
+                this.name = librarianName;
+                this.password = pw;
+                this.success = true;
+            }
+            else 
+            {
+                this.success = false;
+            }
+        }
+        public void removeUser(string userName) 
+        {
+            if (this.success) 
+            {
+
+            }
+        }
+
+        public void listUsers() 
+        {
+        }
+    }
+}
